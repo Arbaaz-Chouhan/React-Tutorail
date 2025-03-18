9f7365e17ae56e268d6da4ef7547c59b38c6d53c
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-03-19 00:46:53 +0530

    react tutorial

--- my-app/src/components/ParentComponent.jsx
+++ my-app/src/components/ParentComponent.jsx
@@ -1,15 +1,15 @@
-// import React from 'react';
-// import ChildComponent from './ChildComponent';
+import React from 'react';
+import ChildComponent from './ChildComponent';
 
-// function ParentComponent() {
-//     const data = "Hello from Parent!";
+function ParentComponent() {
+    const data = "Hello from Parent!";
 
-//     return (
-//         <div>
-//             <h1>Parent Component</h1>
-//             <ChildComponent message={data} />
-//         </div>
-//     );
-// }
+    return (
+        <div>
+            <h1>Parent Component</h1>
+            <ChildComponent message={data} />
+        </div>
+    );
+}
 
-// export default ParentComponent;
+export default ParentComponent;
